7becd93857695dccde09fa79248daa866f1a8fed
shella <dimasshella04@gmail.com>
2019-10-10 22:37:10 +0700

    Android UI

---
+++ app/src/main/java/polinema/ac/id/androiduistarter/fragments/DiscountFragment.java
@@ -0,0 +1,26 @@
+package polinema.ac.id.androiduistarter.fragments;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import polinema.ac.id.androiduistarter.R;
+
+public class DiscountFragment extends Fragment {
+
+    public DiscountFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_discount, container, false);
+    }
+
+}
